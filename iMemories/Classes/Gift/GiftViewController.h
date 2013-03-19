3b67b27175a9f75f80a74d60eb53193c9e72e1fe
mamunboney <boney@surroundapps.com>
2013-03-19 14:25:27 +0600

    added

---
+++ iMemories/Classes/Gift/GiftViewController.h
@@ -0,0 +1,13 @@
+//
+//  GiftViewController.h
+//  iMemories
+//
+//  Created by Lion Boney on 2/14/13.
+//  Copyright (c) 2013 surroundapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GiftViewController : UIViewController
+
+@end
---
+++ iMemories/Classes/Gift/GiftViewController.m
@@ -0,0 +1,39 @@
+//
+//  GiftViewController.m
+//  iMemories
+//
+//  Created by Lion Boney on 2/14/13.
+//  Copyright (c) 2013 surroundapps. All rights reserved.
+//
+
+#import "GiftViewController.h"
+
+@interface GiftViewController ()
+
+@end
+
+@implementation GiftViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"My Gift", nil);
+        self.tabBarItem.image = [UIImage imageNamed:@"gift"];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
