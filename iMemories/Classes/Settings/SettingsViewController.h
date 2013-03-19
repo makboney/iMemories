36bbac1c6f9e6760972098a4f1560db2b8774ef2
mamunboney <boney@surroundapps.com>
2013-03-19 14:24:26 +0600

    added

---
+++ iMemories/Classes/Settings/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  iMemories
+//
+//  Created by Lion Boney on 2/14/13.
+//  Copyright (c) 2013 surroundapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ iMemories/Classes/Settings/SettingsViewController.m
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.m
+//  iMemories
+//
+//  Created by Lion Boney on 2/14/13.
+//  Copyright (c) 2013 surroundapps. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"Settings", @"Settings");
+        self.tabBarItem.image = [UIImage imageNamed:@"gear2"];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
